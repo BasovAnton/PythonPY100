ee6ed5f0a657d5afe4c353e2a77091434c9d4933
antbas <anton.iv90@mail.ru>
2021-10-20 21:17:06 +0300

    Вып. классная работа 20.10.2021

--- Занятие3/Лабораторные_задания/task1_1/main.py
+++ Занятие3/Лабораторные_задания/task1_1/main.py
@@ -1,3 +1,16 @@
+
+def main():
+    sum_ = 0
+    n = 1
+    while True:
+        sum_ = sum_+n**2
+        print(n, sum_)
+        next_ = (n+1)**2
+        if sum_ + next_ >= 500:
+            break
+        n += 1
+    return n
+
+
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    print(main())
--- Занятие3/Лабораторные_задания/task1_6/main.py
+++ Занятие3/Лабораторные_задания/task1_6/main.py
@@ -1,3 +1,15 @@
+def strong_life(a,b):
+    for i in range(0,10):
+        money = 0
+        pov = 1.03*b
+        money += pov-a
+        b = 1.03*i*b
+        print(b)
+    return money
+
+
+
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    print(strong_life(5,6))
+
+
