f4b0a819e7ef25e5d7914957b26766a12a2b33f8
Anton Basov <anton.iv90@mail.ru>
2021-10-22 11:03:14 +0300

    Дз

--- Занятие3/Лабораторные_задания/task1_2/main.py
+++ Занятие3/Лабораторные_задания/task1_2/main.py
@@ -2,10 +2,9 @@ def factorial(n):
     n_fact = 1
     for i in range(1, n+1):
         n_fact *= i
-        print(n_fact)
     return n_fact
 
 
 if __name__ == "__main__":
     # Write your solution here
-    print (factorial(5))
+    print(factorial(5))
