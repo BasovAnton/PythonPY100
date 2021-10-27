b19627c02c433c6d0992d7d368e176fa4a48b032
antbas <anton.iv90@mail.ru>
2021-10-27 19:44:44 +0300

    Вып. классная работа 27.10.2021 часть 1

--- Занятие4/Лабораторные_задания/task1_2/main.py
+++ Занятие4/Лабораторные_задания/task1_2/main.py
@@ -1,3 +1,5 @@
 if __name__ == "__main__":
     # Write your solution here
-    pass
+    def kvadrat(n,m):
+        return [i**2 for i in range(n,m+1) if i%2 ==1]
+    print (kvadrat(1,10))
--- Занятие4/Лабораторные_задания/task1_3/main.py
+++ Занятие4/Лабораторные_задания/task1_3/main.py
@@ -1,3 +1,12 @@
 if __name__ == "__main__":
     # Write your solution here
-    pass
+    def kvadrat(n, m):
+        s = 0
+        for i in range(n, m+1):
+
+            if i % 2 == 0:
+                s += 1
+        return s
+
+
+    print(kvadrat(3, 25))
