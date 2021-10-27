fef14a48cd4f42f8ab1209f0d834f7b3fef86ce5
antbas <anton.iv90@mail.ru>
2021-10-27 20:53:56 +0300

    Упражнение 4

--- Занятие4/Лабораторные_задания/task1_4/main.py
+++ Занятие4/Лабораторные_задания/task1_4/main.py
@@ -1,3 +1,9 @@
 if __name__ == "__main__":
     # Write your solution here
-    pass
+    def func(n, m):
+
+        sred = sum(range(n, m+1))/len(range(n, m+1))
+        return [i  for i in range(n, m+1) if i>sred]
+
+
+    print(func(1, 2))
--- Занятие4/Лабораторные_задания/task1_5/main.py
+++ Занятие4/Лабораторные_задания/task1_5/main.py
@@ -1,2 +1,10 @@
 if __name__ == "__main__":
     list_ = [4, -1, 10, -1, 3, -3, -6, 8, 6, 9]
+    list1=[i for i in list_ if i%2==0]
+    list2 = [i for i in list_ if i%2 != 0]
+    if len(list2)>len(list1):
+        print("Нечетных больше")
+    elif len(list2)<len(list1):
+        print("Четных больше")
+    else:
+        print("Равно")
